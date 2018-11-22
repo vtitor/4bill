004203b0dca8da5962cd4b76237da2738eb228e7
Victor Titor <vtitor.edumix@gmail.com>
2018-11-22 18:08:15 +0200

    Create draft-stuff

---
+++ app.py
@@ -0,0 +1,48 @@
+import time
+
+from flask import Flask, jsonify
+
+from config import AMOUNT_LIMITS_CONFIG
+
+app = Flask(__name__)
+
+limits = dict(sorted(AMOUNT_LIMITS_CONFIG.items()))
+max_limit = max(AMOUNT_LIMITS_CONFIG)
+
+
+class requests:
+    _storage = []
+
+    @classmethod
+    def register(cls, amount):
+        cls._storage.append((amount, time.time()))
+
+    @classmethod
+    def purge(cls):
+        actual_time = time.time() - max_limit
+        for i, (_, _time) in enumerate(cls._storage):
+            if _time >= actual_time:
+                cls._storage = cls._storage[i:]
+
+    @classmethod
+    def verify(cls, new_amount):
+        cls.purge()
+        sums, now = {interval: 0 for interval in limits}, time.time()
+        for _amount, _time in cls._storage:
+            dt = now - _time
+            for interval, limit in limits.items():
+                if dt < interval:
+                    sums[interval] += _amount
+                    if sums[interval] + new_amount > limit:
+                        return f'{interval}/{limit}'
+
+
+@app.route('/request/<int:amount>')
+def request(amount):
+    limit = requests.verify(amount)
+    if limit:
+        result = {'error': f'amount limit exceeded ({limit}sec)'}
+    else:
+        result = {'result': 'OK'}
+        requests.register(amount)
+    return jsonify(result)
